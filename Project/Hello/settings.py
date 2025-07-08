579001fd88434cb77d37d8be857ee6fc499c413d
Riyocodeart <riyanaikdhure855@gmail.com>
2025-07-08 10:20:27 +0530

    frontend development

--- Project/Hello/settings.py
+++ Project/Hello/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # added manually 
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]                                if using global static (static not inside the app but out in project)
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
